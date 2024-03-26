83be11224331a03d63595c884586995b58e9e3f9
Mohsanul786 <mdmohsanul@gmail.com>
2024-03-26 22:41:34 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-semibold text-blue-600">hdoijvdocjm</h1>
+    </>
+  );
+}
+
+export default App;
